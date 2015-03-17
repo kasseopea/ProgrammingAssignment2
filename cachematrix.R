18e7641dbc27ff14b74c584a3b40730b40951c72
kasseopea <kasseopea@gmail.com>
2015-03-17 14:29:26 +0800

    test commit

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,20 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(solve) x <<- solve
+  get <- function() x
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+  
+  
 }
 
 
