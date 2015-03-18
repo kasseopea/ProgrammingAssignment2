0c68950775037252c96dc9ed0c4600be421f0566
kasseopea <kasseopea@gmail.com>
2015-03-18 09:52:13 +0800

    Added comments to code

--- cachematrix.R
+++ cachematrix.R
@@ -1,40 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## function makeCacheMatrix creates a list using the matrix x. In this function 
+## it returns accessor and modifier functions that help retrieve the x matrix or its inverse matrix (invMatrix)
 
 makeCacheMatrix <- function(x = matrix()) {
+  #initialises the inverse matrix to null
   invMatrix <- NULL
+  
+  #modifier and accessor functions of matrix x
   set <- function(y) {
     x <<- y
     invMatrix <<- NULL
   }
-  get <- function() x
-  setInvMat <- function(y) invMatrixl <<- y
-  getInvMat <- function() invMatrix
+  get <- function() x 
+  
+  #modifier and accessor functions of inverse matrix
+  setInvMat <- function(y) invMatrix <<- y  
+  getInvMat <- function() invMatrix  
+  
+  #returns the list of functions
   list(set = set, get = get,
        setInvMat = setInvMat,
        getInvMat = getInvMat)
 }
-  
 
-## Write a short comment describing this function
+
+## cacheSolve uses the R solve() function to calculate the inverse matrix of x
+## if it's not been calculated yet. It will calculate the inverse matrix and store the inverse matrix as the 
+## invMatrix variable of x
+## the function cacheSolve assumes that the list imported has all the accessor or modifier
+## functions to set or get the variable x or its invMatrix
 
 cacheSolve <- function(x, ...) {
   
-    invMat <- x$getInvMat()
-    if(!is.null(invMat)) {
-      message("getting cached data")
-    }
-    else{
-      mat <- x$get()
-      message("No cache, calculating...")
-      invMat <- solve(mat, ...)
-      x$setInvMat(invMat)
-    }
-    invMat
+  invMat <- x$getInvMat()
+  if(!is.null(invMat)) {
+    message("getting cached data")  #there's already an inverse matrix for x
+  } else {
+    mat <- x$get()
+    message("No cached inverse matrix, calculating for you...") #no cached matrix, calculate it and tell the user
+    invMat <- solve(mat, ...)  #calculate the inverse matrix 
+    x$setInvMat(invMat) #set invMat as the inverse matrix for x 
   }
-  
-  
+  invMat #return inverse matrix
+}
+
+
 
 
